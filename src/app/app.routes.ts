48106e8c3940240f8fda619db4e783bb2593b92a
Tymko <kondrashko13@proton.me>
2025-06-06 19:11:40 +0300

    add tests and texts components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import {HomeComponent} from './home/home.component';
 import {authGuard} from './_auth/auth.guard';
 import {TranslateComponent} from './translate/translate.component';
 import {LoginComponent} from './login/login.component';
+import {TextsComponent} from './texts/texts.component';
+import {TestsComponent} from './tests/tests.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, canActivate: [authGuard]},
   {path: 'translate', component: TranslateComponent},
   {path: 'auth', component: LoginComponent},
+  {path: 'texts', component: TextsComponent},
+  {path: 'tests', component: TestsComponent},
   {path: '**', redirectTo: 'home'}
 ];
---
+++ src/app/texts/texts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-texts',
+  imports: [],
+  templateUrl: './texts.component.html',
+  styleUrl: './texts.component.scss'
+})
+export class TextsComponent {
+
+}
