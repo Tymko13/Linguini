1ac1c1ed0fd1a3777dae31cf415e7d8860a0e8ce
Niojen <d.horiachok@ukma.edu.ua>
2025-06-07 14:44:13 +0300

    Add avatar

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../_auth/auth.service';
+import { ImageService } from '../_services/image.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
-  constructor(private router: Router, private auth: AuthService) {}
+export class ProfileComponent implements OnInit {
+  avatarUrl: string = '';
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private imageService: ImageService
+  ) {}
 
   get username() {
     return this.auth.getUsername();
+  }
+
+  ngOnInit() {
+    const savedPath = localStorage.getItem('userAvatar') ?? 'skintones/s_default.PNG';
+
+    this.imageService.getAvatar(savedPath).then(url => {
+      this.avatarUrl = url;
+    });
   }
 
   goBack() {
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,34 +1,46 @@
-import {Component, inject} from '@angular/core';
-import {AuthService} from '../_auth/auth.service';
-import {Router, RouterLink, RouterLinkActive} from '@angular/router';
-import {AsyncPipe, NgIf} from '@angular/common';
-import {ToastService} from '../_services/toast.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { AuthService } from '../_auth/auth.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ToastService } from '../_services/toast.service';
+import { ImageService } from '../_services/image.service';
 
 @Component({
   selector: 'app-sidebar',
+  standalone: true,
   imports: [
     RouterLink,
+    RouterLinkActive,
     NgIf,
-    AsyncPipe,
-    RouterLinkActive
+    AsyncPipe
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   authService = inject(AuthService);
   router = inject(Router);
   toast = inject(ToastService);
+  imageService = inject(ImageService);
+
+  avatarUrl: string = '';
+
+  ngOnInit(): void {
+    const savedAvatar = localStorage.getItem('userAvatar') ?? 'skintones/s_default.PNG';
+
+    this.imageService.getAvatar(savedAvatar).then(url => {
+      this.avatarUrl = url;
+    });
+  }
 
   goToProfile() {
     this.router.navigate(['/profile']);
   }
 
-  onLogout(){
+  onLogout() {
     this.authService.logout().subscribe({
       next: () => { this.router.navigate(['auth']) },
       error: () => { this.toast.show("УПС, схоже ви тут на завжди!", false) }
     });
   }
 }
-
