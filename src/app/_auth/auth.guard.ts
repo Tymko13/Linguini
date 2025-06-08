d5fbdd84ba4d229bbca4e7be7bf675a196535ad1
Tymko <kondrashko13@proton.me>
2025-06-08 20:56:05 +0300

    make authguard navigate to auth on error

--- src/app/_auth/auth.guard.ts
+++ src/app/_auth/auth.guard.ts
@@ -1,16 +1,19 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {Auth, onAuthStateChanged} from '@angular/fire/auth';
 import {inject} from '@angular/core';
+import {ToastService} from '../_services/toast.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const auth = inject(Auth);
   const router = inject(Router);
+  const toast = inject(ToastService);
   return new Promise(resolve => {
     onAuthStateChanged(auth, user => {
       if (user) {
         resolve(true);
       } else {
-        router.navigate(['/translate']);
+        router.navigate(['/auth']);
+        toast.show("Please login to access that page.", false);
         resolve(false);
       }
     });
