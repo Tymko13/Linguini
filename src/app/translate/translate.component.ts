14eeb143c341824c32f5ffbd78b111ac820757d7
Tymko <kondrashko13@proton.me>
2025-06-08 20:58:10 +0300

    add translate page WIP

--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-translate',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './translate.component.html',
   styleUrl: './translate.component.scss'
 })
 export class TranslateComponent {
+  from = "ukr";
+  to = "en";
+  translation = ['I', 'LOVE', 'JavaScript'];
 
+  codeToLang(code: string): string {
+    switch(code) {
+      case "en": return "Англійська";
+      case "ukr": return "Українська";
+      default: return "Автоматично";
+    }
+  }
 }
