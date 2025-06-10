5be7f46df5cd149e8828abd0d3d55f5e5486d48d
Tymko <kondrashko13@proton.me>
2025-06-10 14:01:51 +0300

    delete unneeded component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import {LoginComponent} from './login/login.component';
 import {TextsComponent} from './texts/texts.component';
 import {TestsComponent} from './tests/tests.component';
 import { ProfileComponent } from './profile/profile.component';
-import { EditAvatarComponent } from './edit-avatar/edit-avatar.component';
 
 
 export const routes: Routes = [
@@ -16,6 +15,5 @@ export const routes: Routes = [
   {path: 'auth', component: LoginComponent},
   {path: 'texts', component: TextsComponent},
   {path: 'tests', component: TestsComponent},
-  {path: 'edit-avatar', component: EditAvatarComponent },
   {path: '**', redirectTo: 'home'}
 ];
